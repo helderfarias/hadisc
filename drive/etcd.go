2bbb3d6f72069dff5c1bab4e64e4629eba122f23
helderfarias <helderfarias@gmail.com>
2015-03-09 09:13:29 -0300

    changes for new api

--- drive/etcd.go
+++ drive/etcd.go
@@ -72,20 +72,15 @@ func services(client *etcd.Client) []helper.Service {
 			appType := app.Key[strings.LastIndex(app.Key, "/")+1:]
 
 			if appType == "domain" {
-				for _, item := range app.Nodes {
-					service.Domain = makeDomain(app.Key)
-					service.Endpoint = item.Value
-				}
+				service.Domain = makeDomain(app.Key)
+				service.Endpoint = app.Value
 			}
 
 			if appType == "backend" {
 				for _, item := range app.Nodes {
 					container := item.Key[strings.LastIndex(item.Key, "/")+1:]
-
-					if item.Nodes != nil && item.Nodes.Len() != 0 {
-						server := item.Nodes[0].Value
-						service.Backends = append(service.Backends, helper.Backend{Container: container, Server: server})
-					}
+					server := item.Value
+					service.Backends = append(service.Backends, helper.Backend{Container: container, Server: server})
 				}
 			}
 		}
