50fd2a57e6610580fe637fa81a8ca7a7906dea58
Helder Farias <helderfarias@gmail.com>
2016-05-20 20:38:58 -0300

    fix etd

--- scripts/etcdv2.sh
+++ scripts/etcdv2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 2380:2380 -p 2379:2379 --name etcd coreos/etcd:v2.0.0 -name infra0 \
+docker run -d -p 2380:2380 -p 2379:2379 --name etcd quay.io/coreos/etcd:v2.0.0 -name infra0 \
  -initial-advertise-peer-urls http://0.0.0.0:2380 \
  -listen-client-urls http://0.0.0.0:2379 \
  -listen-peer-urls http://0.0.0.0:2380 \
